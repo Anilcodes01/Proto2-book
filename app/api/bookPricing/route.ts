3551ddf6162bfe5af9f5742d11c77ce7442860c0
Anil Pal <anilcodes01@gmail.com>
2025-04-06 13:32:04 +0530

    fixed vercel errors

--- app/api/bookPricing/route.ts
+++ app/api/bookPricing/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/app/lib/prisma";
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest, ) {
   try {
     const {
       bookProjectId,
--- app/api/get-info/route.ts
+++ app/api/get-info/route.ts
@@ -2,7 +2,7 @@ import prisma from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 
-export async function POST(req: NextRequest, res: NextResponse) {
+export async function POST(req: NextRequest) {
     try {
       const { bookProjectId } = await req.json();
   
