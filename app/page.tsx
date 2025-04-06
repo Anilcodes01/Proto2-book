17ffee82749924cb461f72d27760234c6e4e8757
Anil Pal <anilcodes01@gmail.com>
2025-04-07 00:30:27 +0530

    added route

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   const router = useRouter()
   return (
    <div className="flex items-center justify-center bg-white w-full min-h-screen">
-    <button onClick={() => router.push('/step1')} className="border bg-yellow-300 px-2 py-2 hover:bg-yellow-400 text-black cursor-pointer rounded-lg ">Click me to Start a new book!</button>
+    <button onClick={() => router.push('/dashboard')} className="border bg-yellow-300 px-2 py-2 hover:bg-yellow-400 text-black cursor-pointer rounded-lg ">Click me to Start a new book!</button>
    </div>
   );
 }
